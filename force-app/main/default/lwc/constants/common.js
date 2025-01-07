ca3ba6815dc0ebc01e4060447a98f2c77643c553
scorpionmit01 <alexhochberger@gmail.com>
2025-01-07 12:59:37 -0500

    Rest of Initial Commit

---
+++ force-app/main/default/lwc/constants/common.js
@@ -0,0 +1,22 @@
+export const TOAST_MODE = {
+    ERROR: 'sticky',
+    SUCCESS: 'dismissable',
+    WARNING: 'dismissable',
+    INFO: 'dismissable'
+};
+
+export const TOAST_VARIANT = {
+    ERROR: 'error',
+    SUCCESS: 'success',
+    WARNING: 'warning',
+    INFO: 'info'
+};
+
+export const STEPS = {
+    n1: 'Step 1',
+    n2: 'Step 2',
+    n3: 'Step 3',
+    n4: 'Step 4',
+    n5: 'Step 5',
+    n6: 'Step 6'
+};
